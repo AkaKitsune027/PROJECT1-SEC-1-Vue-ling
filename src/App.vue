c0a69ae4187476903086e9e7a91e6e8770bb001b
Wanassanan <94218479+AkaKitsune027@users.noreply.github.com>
2024-08-10 16:50:50 +0700

    Initial project

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import { ref } from 'vue';
+  const itemRefs = ref([])
+</script>
+ 
+<template>
+<div class="grid grid-cols-4" >
+  <div class="col-start-2 col-span-2 text-center space-y-10">
+  <span class="text-5xl">Mouse Kingdom</span>
+  <div class="grid grid-cols-6 gap-1 w-fit">
+    <div class=" bg-slate-700 w-32 max-w-40 h-28 max-h-36 shadow-lg shadow-cyan-200/20 border" v-for="item in 36" ref="itemRefs">
+      {{ item }}
+  </div>
+  </div>
+</div>
+</div>
+</template>
+ 
+<style scoped>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+import daisyui from 'daisyui';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+    },
+    plugins: [daisyui],
+  }
+}
+
