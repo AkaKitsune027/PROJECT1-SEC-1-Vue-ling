e2df533360efa90a24dbcd27cc4b3759a8957e8d
vee <achmavee.sanu@kmutt.ac.th>
2024-08-11 18:24:28 +0700

    add: Movement Ver2.

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,84 @@
 <script setup>
 import { ref } from 'vue'
 
-const board = ref([
-  ['red', 'red', null, null, null, null],
-  [null, null, null, null, null, null],
-  [null, null, null, null, null, null],
-  [null, null, null, null, null, null],
-  [null, null, null, null, null, null],
-  [null, null, null, null, null, 'black']
-])
+const typesArray = [
+  ...Array(10).fill('plate'),
+  ...Array(7).fill('spring'),
+  ...Array(6).fill('bean'),
+  ...Array(6).fill('cheese'),
+  ...Array(4).fill('mouse trap glue'),
+  ...Array(3).fill('cat')
+]
+
+// สุ่มตำแหน่งของ elements ใน array
+const shuffledTypes = typesArray.sort(() => Math.random() - 0.5)
+
+function generateCards() {
+  const result = []
+
+  for (let r = 0; r < 6; r++) {
+    const row = []
+
+    for (let c = 0; c < 6; c++) {
+      row.push({
+        id: r * 6 + c + 1,
+        type: shuffledTypes.pop(), // นำ type จาก array shuffledTypes มาใช้
+        isReveal: false,
+        pawn:
+          r === 0 && c === 0
+            ? 'red'
+            : r === 0 && c === 1
+            ? 'red'
+            : r === 1 && c === 0
+            ? 'red'
+            : r === 5 && c === 5
+            ? 'black'
+            : r === 5 && c === 4
+            ? 'black'
+            : r === 4 && c === 5
+            ? 'black'
+            : null
+      })
+    }
+
+    result.push(row)
+  }
+  return result
+}
+
+const cards = ref(generateCards())
+
+/* const cards = ref(
+  Array.from({ length: 6 }, (_, rowIndex) =>
+    Array.from({ length: 6 }, (_, cellIndex) => ({
+      id: rowIndex * 6 + cellIndex + 1,
+      type: shuffledTypes.pop(), // นำ type จาก array shuffledTypes มาใช้
+      originalType: shuffledTypes.pop(), // เก็บ type ดั้งเดิมไว้เพื่อใช้งานในอนาคต
+      color: 'gray', // ตั้งค่าสีพื้นฐานเป็นสีเทา
+      pawn:
+        rowIndex === 0 && cellIndex === 0
+          ? 'red'
+          : rowIndex === 0 && cellIndex === 1
+          ? 'red'
+          : rowIndex === 1 && cellIndex === 0
+          ? 'red'
+          : rowIndex === 5 && cellIndex === 5
+          ? 'black'
+          : rowIndex === 5 && cellIndex === 4
+          ? 'black'
+          : rowIndex === 4 && cellIndex === 5
+          ? 'black'
+          : null
+    }))
+  )
+) */
+console.dir(cards.value)
 
 const selectedPawn = ref(null)
 
 const selectCell = (rowIndex, cellIndex) => {
   if (selectedPawn.value === null) {
-    if (board.value[rowIndex][cellIndex] !== null) {
+    if (cards.value[rowIndex][cellIndex].pawn !== null) {
       selectedPawn.value = { row: rowIndex, col: cellIndex }
     }
   } else {
@@ -26,8 +90,19 @@ const selectCell = (rowIndex, cellIndex) => {
 const movePawn = (rowIndex, cellIndex) => {
   const { row, col } = selectedPawn.value
   if (isValidMove(row, col, rowIndex, cellIndex)) {
-    board.value[rowIndex][cellIndex] = board.value[row][col]
-    board.value[row][col] = null
+    const targetCard = cards.value[rowIndex][cellIndex]
+    const fromerCard = cards.value[row][col]
+
+    if (targetCard.isReveal) {
+      targetCard.pawn = fromerCard.pawn
+      fromerCard.pawn = null // เคลื่อนย้ายหมากออกจากช่องเดิม
+    } else {
+      targetCard.isReveal = true
+      setTimeout(() => {
+        targetCard.pawn = fromerCard.pawn
+        fromerCard.pawn = null // เคลื่อนย้ายหมากออกจากช่องเดิม
+      }, 300)
+    }
   }
 }
 
@@ -35,8 +110,6 @@ const isValidMove = (rowFrom, colFrom, rowTo, colTo) => {
   const rowDiff = Math.abs(rowFrom - rowTo)
   const colDiff = Math.abs(colFrom - colTo)
 
-  // การเคลื่อนที่ที่ถูกต้องควรเป็นการเคลื่อนที่ในทิศทางแนวทแยงมุม (1 ช่องหรือ 2 ช่อง)
-  // หรือการเคลื่อนที่ในทิศทางแนวตั้งและแนวนอน (1 ช่องหรือ 2 ช่อง)
   const isDiagonalMove = rowDiff === colDiff && (rowDiff === 1 || rowDiff === 2)
   const isStraightMove =
     (rowDiff === 1 && colDiff === 0) || (rowDiff === 0 && colDiff === 1)
@@ -47,52 +120,36 @@ const isValidMove = (rowFrom, colFrom, rowTo, colTo) => {
 
 <template>
   <div>
-    <div class="board grih-auto">
-      <div v-for="(row, rowIndex) in board" :key="rowIndex" class="row">
-        <div
-          v-for="(cell, cellIndex) in row"
-          :key="cellIndex"
-          class="cell"
-          @click="selectCell(rowIndex, cellIndex)"
-        >
-          <div v-if="cell !== null" :class="['pawn', cell + '-pawn']"></div>
+    <div class="h-screen grid place-items-center">
+      <div class="grid grid-cols-1 grid-rows-6 w-fit">
+        <div v-for="(row, rowIndex) in cards" :key="rowIndex" class="grid grid-cols-6 grid-rows-1">
+          <div
+            v-for="(cell, cellIndex) in row"
+            :key="cell.id"
+            class="w-[75px] h-[75px] flex items-center justify-center border border-white"
+            :class="[
+              !cell.isReveal ? 'bg-gray-800' : '',
+              cell.isReveal && cell.type === 'plate' ? 'bg-yellow-500' : '',
+              cell.isReveal && cell.type === 'spring' ? 'bg-blue-500' : '',
+              cell.isReveal && cell.type === 'bean' ? 'bg-red-500' : '',
+              cell.isReveal && cell.type === 'cheese' ? 'bg-yellow-300' : '',
+              cell.isReveal && cell.type === 'mouse trap glue' ? 'bg-gray-600' : '',
+              cell.isReveal && cell.type === 'cat' ? 'bg-purple-500' : ''
+            ]"
+            @click="selectCell(rowIndex, cellIndex)"
+          >
+            <div
+              v-if="cell.pawn !== null"
+              :class="[
+                'w-10 h-10 rounded-full',
+                cell.pawn === 'red' ? 'bg-red-500' : 'bg-black'
+              ]"
+            ></div>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<style scoped>
-.board {
-  height:100ch;
-  justify-content:center;
-  display: grid;
-  grid-template-columns: repeat(6, 75px);
-  grid-template-rows: repeat(6, 75px);
-}
-
-.cell {
-  width: 75px;
-  height: 75px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border: solid;
-  border-color: white;
-  background-color: #333;
-}
-
-.pawn {
-  width: 40px;
-  height: 40px;
-  border-radius: 50%;
-}
-
-.red-pawn {
-  background-color: red;
-}
-
-.black-pawn {
-  background-color: black;
-}
-</style>
+<style scoped></style>
