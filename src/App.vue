8b4c69bd6fe86b37a45ce2d96a283c2a4347c7cb
vee <achmavee.sanu@kmutt.ac.th>
2024-08-11 13:31:53 +0700

    add: Movement ver.1

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,98 @@
 <script setup>
-  import { ref } from 'vue';
-  const itemRefs = ref([])
-</script>
- 
-<template>
-<div class="grid grid-cols-4" >
-  <div class="col-start-2 col-span-2 text-center space-y-10">
-  <span class="text-5xl">Mouse Kingdom</span>
-  <div class="grid grid-cols-6 gap-1 w-fit">
-    <div class=" bg-slate-700 w-32 max-w-40 h-28 max-h-36 shadow-lg shadow-cyan-200/20 border" v-for="item in 36" ref="itemRefs">
-      {{ item }}
-  </div>
-  </div>
-</div>
-</div>
-</template>
- 
-<style scoped>
+import { ref } from 'vue'
 
+const board = ref([
+  ['red', 'red', null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, 'black']
+])
+
+const selectedPawn = ref(null)
+
+const selectCell = (rowIndex, cellIndex) => {
+  if (selectedPawn.value === null) {
+    if (board.value[rowIndex][cellIndex] !== null) {
+      selectedPawn.value = { row: rowIndex, col: cellIndex }
+    }
+  } else {
+    movePawn(rowIndex, cellIndex)
+    selectedPawn.value = null
+  }
+}
+
+const movePawn = (rowIndex, cellIndex) => {
+  const { row, col } = selectedPawn.value
+  if (isValidMove(row, col, rowIndex, cellIndex)) {
+    board.value[rowIndex][cellIndex] = board.value[row][col]
+    board.value[row][col] = null
+  }
+}
+
+const isValidMove = (rowFrom, colFrom, rowTo, colTo) => {
+  const rowDiff = Math.abs(rowFrom - rowTo)
+  const colDiff = Math.abs(colFrom - colTo)
+
+  // การเคลื่อนที่ที่ถูกต้องควรเป็นการเคลื่อนที่ในทิศทางแนวทแยงมุม (1 ช่องหรือ 2 ช่อง)
+  // หรือการเคลื่อนที่ในทิศทางแนวตั้งและแนวนอน (1 ช่องหรือ 2 ช่อง)
+  const isDiagonalMove = rowDiff === colDiff && (rowDiff === 1 || rowDiff === 2)
+  const isStraightMove =
+    (rowDiff === 1 && colDiff === 0) || (rowDiff === 0 && colDiff === 1)
+
+  return isDiagonalMove || isStraightMove
+}
+</script>
+
+<template>
+  <div>
+    <div class="board grih-auto">
+      <div v-for="(row, rowIndex) in board" :key="rowIndex" class="row">
+        <div
+          v-for="(cell, cellIndex) in row"
+          :key="cellIndex"
+          class="cell"
+          @click="selectCell(rowIndex, cellIndex)"
+        >
+          <div v-if="cell !== null" :class="['pawn', cell + '-pawn']"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.board {
+  height:100ch;
+  justify-content:center;
+  display: grid;
+  grid-template-columns: repeat(6, 75px);
+  grid-template-rows: repeat(6, 75px);
+}
+
+.cell {
+  width: 75px;
+  height: 75px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: solid;
+  border-color: white;
+  background-color: #333;
+}
+
+.pawn {
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+}
+
+.red-pawn {
+  background-color: red;
+}
+
+.black-pawn {
+  background-color: black;
+}
 </style>
